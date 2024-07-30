59812c44d3a3fa314e5f759899fa432e6c2a107f
1996jessie <jessie1996@naver.com>
2024-07-30 16:57:37 +0900

    Title: 정수 선언하고 곱 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 26;
+    int b = 5;
+    printf("%d", a);
+    printf(" * ");
+    printf("%d", b);
+    printf(" = ");
+    printf("%d", a * b);
+    return 0;
+}
