61e53cb1277ac4c98bbcbf64ee5f3497e4ee1f03
1996jessie <jessie1996@naver.com>
2024-07-30 16:52:51 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/정수 선언하고 뺄셈/define-numbers-and-substract.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    printf("%d", 97);
+    printf(" - ");
+    printf("%d", 13);
+    printf(" = ");
+    printf("%d", 84);
+    return 0;
+}
