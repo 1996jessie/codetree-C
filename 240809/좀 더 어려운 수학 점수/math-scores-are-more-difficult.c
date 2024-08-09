7aac940f9f2ab8a97ddd7e60bba60a4f5469de50
1996jessie <jessie1996@naver.com>
2024-08-09 17:34:27 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/좀 더 어려운 수학 점수/math-scores-are-more-difficult.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int am, ae, bm, be;
+    scanf("%d %d\n", &am, &ae);
+    scanf("%d %d\n", &bm, &be);
+    if(am >= bm) {
+        printf("A");
+    } else if(am < bm) {
+        printf("B");
+    } else {
+        if(ae > be) {
+            printf("A");
+        } else {
+            printf("B");
+        }
+    }
+    return 0;
+}
