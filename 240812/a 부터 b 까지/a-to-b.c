d425a17e84feab5760d2801ecab1c97510bc410c
1996jessie <jessie1996@naver.com>
2024-08-12 17:22:36 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240812/a 부터 b 까지/a-to-b.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    while (a <= b) {
+        printf("%d ", a);
+        if (a % 2 == 1) {
+            a *= 2;  
+        } else {
+            a += 3; 
+        }
+    }
+    
+    return 0;
+}
