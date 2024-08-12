fdc21f8d319fefd2d536763749a31cd130a4cd91
1996jessie <jessie1996@naver.com>
2024-08-12 17:27:20 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240812/369 게임/369-game.c
+++ 240812/369 게임/369-game.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
+int contains369(int num) {
+    while (num > 0) {
+        int digit = num % 10;
+        if (digit == 3 || digit == 6 || digit == 9) {
+            return 1;  
+        }
+        num /= 10; 
+    }
+    return 0; 
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d", &n);
-    for(int i=1;i<=n;i++) {
-        if(i % 3 == 0 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+    
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 || contains369(i)) {
             printf("0 ");
         } else {
             printf("%d ", i);
         }
     }
+    
     return 0;
 }
