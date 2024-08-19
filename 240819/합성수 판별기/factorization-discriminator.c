52ddc9dfc0c8f21259741953d82187618922c48e
1996jessie <jessie1996@naver.com>
2024-08-19 13:15:28 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240819/합성수 판별기/factorization-discriminator.c
+++ 240819/합성수 판별기/factorization-discriminator.c
@@ -6,7 +6,7 @@ int main() {
     int n;
     scanf("%d", &n);
     bool flag = false;
-    for(int i=1;i<n;i++) {
+    for(int i=2;i<n;i++) {
         if(n % i == 0) {
             flag = true;
             break;
