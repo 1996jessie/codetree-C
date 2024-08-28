b60e04176cac400f0ff04e57b625a5d6c29f5c94
1996jessie <jessie1996@naver.com>
2024-08-28 22:47:08 +0900

    Title: 별표 출력하기 11, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240828/별표 출력하기 11/print-star-11.c
+++ 240828/별표 출력하기 11/print-star-11.c
@@ -4,11 +4,9 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d", &n);
-
-    int size = 2 * n + 1;
-
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
+    
+    for (int i = 0; i < 2 * n + 1; i++) {
+        for (int j = 0; j < 2 * n + 1; j++) {
             if (i % 2 == 0 || j % 2 == 0) {
                 printf("* ");
             } 
