bc3cb5e07d6bb616d8079016d98b5fb6c21cb0c3
1996jessie <jessie1996@naver.com>
2024-08-28 22:50:40 +0900

    Title: 별표 출력하기 12, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240828/별표 출력하기 12/print-star-12.c
+++ 240828/별표 출력하기 12/print-star-12.c
@@ -5,22 +5,18 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    for (int i = 1; i <= n; i++) {
-        if (i == 1) {
-            for (int j = 1; j <= n; j++) {
-                printf("* ");
-            }
-            printf("\n");
-        } else {
-            for (int j = 1; j < i; j++) {
-                printf("  ");
-            }
-            for (int j = i; j <= n; j++) {
-                printf("*  ");
-                j++;
-            }
-            printf("\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            printf("  ");
         }
+
+        for (int j = 0; j < n - i; j++) {
+            printf("* ");
+            if (j < n - i - 1) {
+                printf(" ");
+            }
+        }
+        printf("\n");
     }
 
     return 0;
