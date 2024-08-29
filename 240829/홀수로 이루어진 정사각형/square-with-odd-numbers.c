fe47d414b91688db14e05ca0be84db2c61b47597
1996jessie <jessie1996@naver.com>
2024-08-29 18:08:44 +0900

    Title: 홀수로 이루어진 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240829/홀수로 이루어진 정사각형/square-with-odd-numbers.c
+++ 240829/홀수로 이루어진 정사각형/square-with-odd-numbers.c
@@ -5,11 +5,9 @@ int main() {
     int n;
     scanf("%d", &n);
 
-    int start_num = 11; 
-
     for (int i = 0; i < n; i++) { 
         for (int j = 0; j < n; j++) { 
-            printf("%d", start_num + 2 * (i + j));
+            printf("%d", 11 + 2 * (i + j));
             if (j < n - 1) {
                 printf(" "); 
             }
