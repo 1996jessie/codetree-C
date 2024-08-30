dcfbe614274b0baa26a116761c7eb07eaa021038
1996jessie <jessie1996@naver.com>
2024-08-30 20:25:34 +0900

    Title: 좌우로 반복하며 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240830/좌우로 반복하며 출력/print-in-left-right-reverse.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n); 
+
+    for (int i = 0; i < n; i++) {  
+        if (i % 2 == 0) { 
+            for (int j = 1; j <= n; j++) {
+                printf("%d", j);
+            }
+        } else {  
+            for (int j = n; j >= 1; j--) {
+                printf("%d", j);
+            }
+        }
+        printf("\n");  
+    }
+
+    return 0;
+}
