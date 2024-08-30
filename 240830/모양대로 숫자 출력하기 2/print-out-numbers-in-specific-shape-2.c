7588fdf89dff0e64a5e3f9151cc2111dba3594e8
1996jessie <jessie1996@naver.com>
2024-08-30 20:18:08 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240830/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n); 
+
+    int even[] = {2, 4, 6, 8};
+
+    for (int i = 0; i < n; i++) { 
+        for (int j = 0; j < n; j++) {
+            printf("%d", even[(i + j) % 4]);
+            if (j < n - 1) {
+                printf(" "); 
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
