be912a0e590c366335f462ab3bc28fbaab2a4298
1996jessie <jessie1996@naver.com>
2024-09-01 21:08:02 +0900

    Title: ab곱 n번 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240901/ab곱 n번 구하기/a-multiple-b-n-times.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n); 
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        scanf("%d %d", &a, &b); 
+
+        long long p = 1;
+        for (int j = a; j <= b; j++) {
+            p *= j;  
+        }
+
+        printf("%lld\n", p);  
+    }
+
+    return 0;
+}
