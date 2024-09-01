78861adb8d145d5724fdee4055818c71d0afac3f
1996jessie <jessie1996@naver.com>
2024-09-01 21:03:16 +0900

    Title: 19단 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240901/19단 출력/nineteen-times-table.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for (int i = 1; i <= 19; i++) {
+        for (int j = 1; j <= 19; j++) {
+            printf("%d * %d = %d", i, j, i * j);
+            if (j % 2 == 0 || j == 19) {
+                printf("\n"); 
+            } else {
+                printf(" / "); 
+            }
+        }
+    }
+    return 0;
+}
