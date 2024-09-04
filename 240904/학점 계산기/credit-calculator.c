74748a3d65f7f9466581a567bc3a20ed02698c2b
1996jessie <jessie1996@naver.com>
2024-09-04 16:15:33 +0900

    Title: 학점 계산기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240904/학점 계산기/credit-calculator.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    double arr[5];
+    double sum = 0.0; 
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%f", &arr[i]);
+        sum += arr[i];
+    }
+
+    double avg = sum / n;
+
+    printf("%.1f\n", avg);
+
+    if (avg >= 4.0) {
+        printf("Perfect\n");
+    } else if (average >= 3.0) {
+        printf("Good\n");
+    } else {
+        printf("Poor\n");
+    }
+
+    return 0;
+}
