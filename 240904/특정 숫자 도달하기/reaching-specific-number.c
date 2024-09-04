92cf87b6afe11152f822908da27713a53a554fb6
1996jessie <jessie1996@naver.com>
2024-09-04 16:13:30 +0900

    Title: 특정 숫자 도달하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240904/특정 숫자 도달하기/reaching-specific-number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    int sum = 0;
+    int count = 10;
+
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &arr[i]);
+        if (arr[i] >= 250) {
+            count = i;
+            break;
+        }
+    }
+
+    for (int i = 0; i < count; i++) {
+        sum += numbers[i];
+    }
+
+    double average = (double)sum / count;
+
+    printf("%d %.1f\n", sum, average);
+
+    return 0;
+}
