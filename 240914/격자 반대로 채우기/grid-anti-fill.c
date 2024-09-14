431c6e652aebd83c7c1924d47d5f65f12228796c
1996jessie <jessie1996@naver.com>
2024-09-14 11:36:50 +0900

    Title: 격자 반대로 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240914/격자 반대로 채우기/grid-anti-fill.c
+++ 240914/격자 반대로 채우기/grid-anti-fill.c
@@ -4,26 +4,61 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d", &n);
-    int num[10][10]={{0}};
-    int len=1;
 
-    for(int j=n-1; j>=0; j--){
-        if(j%2==0){
-            for(int i=0; i<n; i++){
-                num[i][j]=len++;
+    int number = 0;
+    int graph[n][n];
+
+    // Initialize the graph with zeros
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            graph[i][j] = 0;
+        }
+    }
+
+    if (n % 2 == 0) {
+        for (int i = 0; i < n; i++) {
+            int adjusted_i = n - i;
+            if (adjusted_i % 2 == 0) {
+                for (int j = 0; j < n; j++) {
+                    int adjusted_j = n - j;
+                    number++;
+                    graph[adjusted_j - 1][adjusted_i - 1] = number;
+                }
+            } else {
+                for (int k = 0; k < n; k++) {
+                    number++;
+                    graph[k][adjusted_i - 1] = number;
+                }
             }
         }
-        else{
-            for(int i=n-1; i>=0; i--){
-                num[i][j]=len++;
+    } else {
+        for (int i = 0; i < n; i++) {
+            int adjusted_i = n - i;
+            if (adjusted_i % 2 == 0) {
+                for (int k = 0; k < n; k++) {
+                    number++;
+                    graph[k][adjusted_i - 1] = number;
+                }
+            } else {
+                for (int j = 0; j < n; j++) {
+                    int adjusted_j = n - j;
+                    number++;
+                    graph[adjusted_j - 1][adjusted_i - 1] = number;
+                }
             }
         }
     }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            printf("%d ", num[i][j]);
+
+    // Print the graph
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j > 0) {
+                printf(" ");
+            }
+            printf("%d", graph[i][j]);
         }
         printf("\n");
     }
+
     return 0;
 }
