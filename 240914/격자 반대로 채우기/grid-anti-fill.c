b28975aa19e142d69bd8c2c2c97a337f2bd0e28d
1996jessie <jessie1996@naver.com>
2024-09-14 11:33:34 +0900

    Title: 격자 반대로 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240914/격자 반대로 채우기/grid-anti-fill.c
+++ 240914/격자 반대로 채우기/grid-anti-fill.c
@@ -4,38 +4,26 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d", &n);
+    int num[10][10]={{0}};
+    int len=1;
 
-    int matrix[n][n];
-    int num = 1;
-    
-    // 배열을 지그재그 방향으로 채우기
-    for (int diag = 0; diag < 2 * n - 1; ++diag) {
-        int row = (diag < n) ? n - 1 - diag : 0;
-        int col = (diag < n) ? 0 : diag - n + 1;
-        
-        if (diag % 2 == 0) {
-            // 오른쪽 아래에서 위쪽으로
-            for (int i = row, j = col; i < n && j < n; ++i, ++j) {
-                matrix[i][j] = num++;
+    for(int j=n-1; j>=0; j--){
+        if(j%2==0){
+            for(int i=0; i<n; i++){
+                num[i][j]=len++;
             }
-        } else {
-            // 왼쪽 위에서 오른쪽 아래로
-            for (int i = row, j = col; i < n && j < n; ++i, ++j) {
-                matrix[j][i] = num++;
+        }
+        else{
+            for(int i=n-1; i>=0; i--){
+                num[i][j]=len++;
             }
         }
     }
-    
-    // 결과 출력
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (j > 0) {
-                printf(" ");
-            }
-            printf("%d", matrix[i][j]);
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            printf("%d ", num[i][j]);
         }
         printf("\n");
     }
-    
     return 0;
 }
