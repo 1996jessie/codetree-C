39332a23f1d37cdcda4d922b69dbe4e8ef0c4c67
1996jessie <jessie1996@naver.com>
2024-09-17 20:56:46 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240917/단어로 구분하기/separate-words-with-words.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char word[21]; 
+    
+    for (int i = 0; i < 10; i++) {
+        scanf("%s", word); 
+        printf("%s\n", word);
+    }
+
+    return 0;
+}
