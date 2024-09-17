f26056d7a992af8577edf07f3ec6b0e302ff41ad
1996jessie <jessie1996@naver.com>
2024-09-17 21:07:23 +0900

    Title: 문자열에서 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240917/문자열에서 문자 출력/print-chars-from-word.c
+++ 240917/문자열에서 문자 출력/print-chars-from-word.c
@@ -6,7 +6,7 @@ int main() {
     char str[10]; 
     scanf("%s", str); 
     
-    int len = strlen(str);  /
+    int len = strlen(str);
 
     for (int i = 0; i < len; i++) {
         printf("%c\n", str[i]);
