b6e6c75b902ea5ad16b0b5bb296c031388bfd850
1996jessie <jessie1996@naver.com>
2024-09-17 21:07:00 +0900

    Title: 문자열에서 문자 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240917/문자열에서 문자 출력/print-chars-from-word.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char str[10]; 
+    scanf("%s", str); 
+    
+    int len = strlen(str);  /
+
+    for (int i = 0; i < len; i++) {
+        printf("%c\n", str[i]);
+    }
+    
+    return 0;
+}
