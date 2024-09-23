0c72ab25a64dd68301634336142fa94291f5044c
1996jessie <jessie1996@naver.com>
2024-09-23 12:18:48 +0900

    Title: 대문자로 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240923/대문자로 출력하기/print-in-capital.c
+++ 240923/대문자로 출력하기/print-in-capital.c
@@ -3,19 +3,25 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     char str[101];
-    int i = 0;
+    char result[101];
+    int i = 0, j = 0;
 
     scanf("%100s", str);
 
     while (str[i] != '\0') {
-        if (str[i] >= 'A' && str[i] <= 'Z') {
-            putchar(str[i]);
-        }
-        else if (str[i] >= 'a' && str[i] <= 'z') {
-            putchar(str[i] - 'a' + 'A');
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            result[j] = str[i] - ('a' - 'A'); 
+            j++;
+        } else if (str[i] >= 'A' && str[i] <= 'Z') {
+            result[j] = str[i]; 
+            j++;
         }
         i++;
     }
+
+    result[j] = '\0';
+
+    printf("%s\n", result);
 
     return 0;
 }
