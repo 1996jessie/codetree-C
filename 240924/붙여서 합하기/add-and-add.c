f2dfcffc5d2851cdfc5008657c6db894bd529121
1996jessie <jessie1996@naver.com>
2024-09-24 23:02:31 +0900

    Title: 붙여서 합하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240924/붙여서 합하기/add-and-add.c
+++ 240924/붙여서 합하기/add-and-add.c
@@ -4,11 +4,36 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     char A[10], B[10];
-
     scanf("%s %s", A, B);
 
-    int num_AB = atoi(A) * 1000 + atoi(B);
-    int num_BA = atoi(B) * 1000 + atoi(A);
+    char AB[20], BA[20];
+
+    int i = 0, j = 0;
+    while (A[i] != '\0') {
+        AB[j++] = A[i++];
+    }
+
+    i = 0;
+    while (B[i] != '\0') {
+        AB[j++] = B[i++];
+    }
+    AB[j] = '\0'; 
+
+    i = 0;
+    j = 0;
+
+    while (B[i] != '\0') {
+        BA[j++] = B[i++];
+    }
+
+    i = 0;
+    while (A[i] != '\0') {
+        BA[j++] = A[i++];
+    }
+    BA[j] = '\0'; 
+
+    int num_AB = atoi(AB);
+    int num_BA = atoi(BA);
 
     int result = num_AB + num_BA;
 
