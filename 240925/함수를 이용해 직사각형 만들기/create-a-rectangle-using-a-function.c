96ee976acd85c91d3d81601a4feaf0a4d6e2d685
1996jessie <jessie1996@naver.com>
2024-09-25 21:18:16 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240925/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void printRectangle(int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("1");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    scanf("%d %d", &n, &m);
+    printRectangle(n, m);
+    return 0;
+}
