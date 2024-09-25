171644c188cbb83aba412860913ecaf91fff5c31
1996jessie <jessie1996@naver.com>
2024-09-25 21:17:28 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240925/반복 출력하기/repeated-output.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void printString(int N) {
+    for (int i = 0; i < N; i++) {
+        printf("12345^&*()_\n");
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요
+    int N;
+    scanf("%d", &N);
+    printString(N);
+    return 0;
+}
