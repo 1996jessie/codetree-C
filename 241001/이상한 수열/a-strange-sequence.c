c14bf6ad353d10581c9152919280fbee04fd8bbb
1996jessie <jessie1996@naver.com>
2024-10-01 17:16:43 +0900

    Title: 이상한 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241001/이상한 수열/a-strange-sequence.c
+++ 241001/이상한 수열/a-strange-sequence.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
 
-int sequence(int n) {
-    if (n == 1) {
+int StrangeSequence(int a) {
+    if (a == 1)
         return 1;
-    }
-    if (n == 2) {
+    if (a == 2)
         return 2;
-    }
-    
-    int index = 3 * n / 2; 
-    if (index > n - 1) {
-        index = n - 1; 
-    }
-    
-    return sequence(index) + sequence(n - 1);
+
+    return StrangeSequence(a / 3) + StrangeSequence(a - 1);
 }
+
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
     scanf("%d", &n);
 
-    int result = sequence(n);
-    printf("%d\n", result);
+    printf("%d\n", StrangeSequence(n));
 
     return 0;
 }
