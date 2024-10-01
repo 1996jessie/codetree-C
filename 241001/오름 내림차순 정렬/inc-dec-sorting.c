c7c73031bf266071458bd2d3b07f466d8adcb37c
1996jessie <jessie1996@naver.com>
2024-10-01 17:19:34 +0900

    Title: 오름 내림차순 정렬, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241001/오름 내림차순 정렬/inc-dec-sorting.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void sortAscending(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+void sortDescending(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] < arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    scanf("%d", &n); 
+    int arr[n]; 
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    sortAscending(arr, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+
+    sortDescending(arr, n);
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+
+    return 0;
+}
