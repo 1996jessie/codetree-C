126e141e8ce1bd243b278a1486f01174309082a1
1996jessie <jessie1996@naver.com>
2024-10-03 21:23:35 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 241003/정렬된 숫자 위치 알아내기/indices-of-sorted-array.c
+++ 241003/정렬된 숫자 위치 알아내기/indices-of-sorted-array.c
@@ -13,7 +13,6 @@ void bubbleSort(struct Number arr[], int n) {
         for (j = 0; j < n - i - 1; j++) {
             if (arr[j].value > arr[j + 1].value || 
                (arr[j].value == arr[j + 1].value && arr[j].index > arr[j + 1].index)) {
-                // Swap arr[j] and arr[j + 1]
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -28,22 +27,18 @@ int main() {
     struct Number numbers[1000];
     int result[1000];
 
-    // 입력
     scanf("%d", &n);
     for (i = 0; i < n; i++) {
         scanf("%d", &numbers[i].value);
-        numbers[i].index = i; // 원래 인덱스를 저장
+        numbers[i].index = i;
     }
 
-    // 정렬
     bubbleSort(numbers, n);
 
-    // 결과 배열에 이동한 위치 저장
     for (i = 0; i < n; i++) {
-        result[numbers[i].index] = i + 1; // 인덱스 보정
+        result[numbers[i].index] = i + 1;
     }
 
-    // 출력
     for (i = 0; i < n; i++) {
         printf("%d ", result[i]);
     }
