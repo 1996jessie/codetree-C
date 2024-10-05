6ddbad100b8b807e14226dc5b1a12795aacb6603
1996jessie <jessie1996@naver.com>
2024-10-05 09:52:37 +0900

    Title: 왔다 갔던 구역 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241005/왔다 갔던 구역 2/area-been-to-and-from2.c
+++ 241005/왔다 갔던 구역 2/area-been-to-and-from2.c
@@ -1,40 +1,56 @@
 #include <stdio.h>
 
+int n;
+int x1[100], x2[100];
+
+int checked[2001];
+
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n;
-    int position = 0;
-    int visited[1001] = {0}; 
-    int offset = 500;
-
     scanf("%d", &n);
-
-    for (int i = 0; i < n; i++) {
-        int x;
+    
+    // 현재 위치
+    int cur = 0;
+    
+    for(int i = 0; i < n; i++) {
+        int distance;
         char direction;
-        scanf("%d %c", &x, &direction);
+        scanf("%d %c", &distance, &direction);
+        
+        if(direction == 'L') {
+            // 왼쪽으로 이동할 경우 : cur - distance ~ cur까지 경로 이동
+            x1[i] = cur - distance;
+            x2[i] = cur;
+            cur -= distance;
+        } else {
+            // 오른쪽으로 이동할 경우 : cur ~ cur + distance까지 경로 이동
+            x1[i] = cur;
+            x2[i] = cur + distance;
+            cur += distance;
+        }
 
-        if (direction == 'R') {
-            for (int j = 1; j <= x; j++) {
-                position++;
-                visited[position + offset]++;
-            }
-        } else if (direction == 'L') {
-            for (int j = 1; j <= x; j++) {
-                position--;
-                visited[position + offset]++;
-            }
+        // OFFSET을 더해줍니다. (OFFSET = 1000)
+        x1[i] += 1000;
+        x2[i] += 1000;
+    }
+    
+    // 구간을 칠해줍니다.
+    // 구간 단위로 진행하는 문제이므로
+    // x2[i]에 등호가 들어가지 않음에 유의합니다.
+    for(int i = 0; i < n; i++) {
+        for(int j = x1[i]; j < x2[i]; j++) {
+            checked[j]++;
         }
     }
-
-    int count = 0;
-    for (int i = 0; i < 1001; i++) {
-        if (visited[i] >= 2) {
-            count++;
+    
+    // 2번 이상 지나간 영역의 크기를 구합니다.
+    int cnt = 0;
+    for(int i = 0; i <= 2000; i++) {
+        if(checked[i] >= 2) {
+            cnt++;
         }
     }
-
-    printf("%d\n", count);
-
+    
+    printf("%d\n", cnt);
     return 0;
 }
