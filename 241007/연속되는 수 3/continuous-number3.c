9dcaa3ed037733db850fd73a5df7b90c140f1c5e
1996jessie <jessie1996@naver.com>
2024-10-07 11:28:29 +0900

    Title: 연속되는 수 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241007/연속되는 수 3/continuous-number3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int N;
+    scanf("%d", &N); 
+
+    int max_len = 1;
+    int current_len = 1;
+    int prev, current;
+
+    scanf("%d", &prev); 
+
+    for (int i = 1; i < N; i++) {
+        scanf("%d", &current);  
+        
+        if ((prev > 0 && current > 0) || (prev < 0 && current < 0)) {
+            current_len++;  
+        } else {
+            if (current_len > max_len) {
+                max_len = current_len;
+            }
+            current_len = 1;
+        }
+        
+        prev = current;  
+    }
+
+    if (current_len > max_len) {
+        max_len = current_len;
+    }
+
+    printf("%d\n", max_len);
+
+    return 0;
+}
