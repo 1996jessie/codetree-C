db51c330cab83d34653ae0dc6a0122c1278ad8d0
1996jessie <jessie1996@naver.com>
2024-10-07 11:54:17 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241007/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.c
+++ 241007/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.c
@@ -11,11 +11,10 @@ typedef struct {
     int person2;
 } Shake;
 
-// 비교 함수 for qsort
 int cmp(const void *a, const void *b) {
     Shake *shakeA = (Shake *)a;
     Shake *shakeB = (Shake *)b;
-    return shakeA->time - shakeB->time; // 시간에 따라 오름차순 정렬
+    return shakeA->time - shakeB->time; 
 }
 
 int main() {
@@ -23,26 +22,22 @@ int main() {
     int n, k, p, t;
     scanf("%d %d %d %d", &n, &k, &p, &t);
     
-    bool infected[MAX_N + 1] = {false}; // 감염된 개발자
-    int shake_num[MAX_N + 1] = {0}; // 각 개발자의 악수 횟수
-    infected[p] = true; // 초기 감염자 설정
+    bool infected[MAX_N + 1] = {false};
+    int shake_num[MAX_N + 1] = {0}; 
+    infected[p] = true;
 
     Shake shakes[MAX_T];
 
-    // 악수 정보 입력
     for (int i = 0; i < t; i++) {
         scanf("%d %d %d", &shakes[i].time, &shakes[i].person1, &shakes[i].person2);
     }
 
-    // qsort를 사용한 정렬
     qsort(shakes, t, sizeof(Shake), cmp);
 
-    // 각 악수 횟수를 세고 감염 처리
     for (int i = 0; i < t; i++) {
         int target1 = shakes[i].person1;
         int target2 = shakes[i].person2;
 
-        // 감염되어 있을 경우 악수 횟수를 증가시킵니다.
         if (infected[target1]) {
             shake_num[target1]++;
         }
@@ -50,18 +45,15 @@ int main() {
             shake_num[target2]++;
         }
 
-        // target1이 감염되어 있고 아직 K번 이하로 악수했다면 target2를 전염시킵니다.
         if (shake_num[target1] <= k && infected[target1]) {
             infected[target2] = true;
         }
 
-        // target2가 감염되어 있고 아직 K번 이하로 악수했다면 target1을 전염시킵니다.
         if (shake_num[target2] <= k && infected[target2]) {
             infected[target1] = true;
         }
     }
 
-    // 결과 출력
     for (int i = 1; i <= n; i++) {
         printf("%d", infected[i] ? 1 : 0);
     }
