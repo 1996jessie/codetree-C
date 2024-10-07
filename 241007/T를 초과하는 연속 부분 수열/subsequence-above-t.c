9a0506e74144c5ccd659b29de3a43f24f94d3e0d
1996jessie <jessie1996@naver.com>
2024-10-07 11:35:18 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241007/T를 초과하는 연속 부분 수열/subsequence-above-t.c
+++ 241007/T를 초과하는 연속 부분 수열/subsequence-above-t.c
@@ -13,25 +13,21 @@ int main() {
     int max_len = 0;  
     int current_len = 0; 
 
-    // 수열을 순회하며 조건을 만족하는 부분 수열을 찾음
     for (int i = 0; i < n; i++) {
         if (arr[i] > t) {
-            current_len++;  // t보다 크면 현재 수열 길이를 증가
+            current_len++;  
         } else {
-            // t보다 크지 않으면 현재까지의 길이와 최대 길이를 비교하여 갱신
             if (current_len > max_len) {
                 max_len = current_len;
             }
-            current_len = 0;  // 새로운 수열을 시작
+            current_len = 0; 
         }
     }
 
-    // 마지막 수열의 길이를 한 번 더 체크
     if (current_len > max_len) {
         max_len = current_len;
     }
 
-    // 결과 출력
     printf("%d\n", max_len);
 
     return 0;
