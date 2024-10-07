35f5b6bbe7aaa39fe89640889cb4190af62fd2bc
1996jessie <jessie1996@naver.com>
2024-10-07 11:43:31 +0900

    Title: 선두를 지켜라, Time: 10ms, Memory: 3MB, Status: Passed - CodeTree

--- 241007/선두를 지켜라/keep-the-lead.c
+++ 241007/선두를 지켜라/keep-the-lead.c
@@ -1,70 +1,50 @@
 #include <stdio.h>
 
+#define MAX_T 1000000
+
+int n, m;
+int pos_a[MAX_T + 1], pos_b[MAX_T + 1];
+
 int main() {
     // 여기에 코드를 작성해주세요.
-    int N, M;
-    scanf("%d %d", &N, &M);
-
-    // A와 B의 이동 정보를 저장할 배열
-    int A_vel[N], A_time[N], B_vel[M], B_time[M];
-
-    // A의 이동 정보 입력
-    for (int i = 0; i < N; i++) {
-        scanf("%d %d", &A_vel[i], &A_time[i]);
-    }
-
-    // B의 이동 정보 입력
-    for (int i = 0; i < M; i++) {
-        scanf("%d %d", &B_vel[i], &B_time[i]);
-    }
-
-    int lead_changes = 0;  // 선두가 바뀐 횟수
-    int A_dist = 0, B_dist = 0;  // A와 B의 누적 이동 거리
-    int A_idx = 0, B_idx = 0;    // A와 B의 현재 구간 인덱스
-    int A_rem_time = A_time[A_idx], B_rem_time = B_time[B_idx];  // A와 B의 남은 시간
-    int last_lead = 0;  // 0: 동등, 1: A가 선두, -1: B가 선두
-
-    // A와 B가 모든 구간을 처리할 때까지 반복
-    while (A_idx < N && B_idx < M) {
-        // 이번 구간에서 처리할 최소 시간을 구한다
-        int move_time = (A_rem_time < B_rem_time) ? A_rem_time : B_rem_time;
-
-        // A와 B가 move_time 동안 이동한 거리를 누적
-        A_dist += A_vel[A_idx] * move_time;
-        B_dist += B_vel[B_idx] * move_time;
-
-        // 선두 비교
-        if (A_dist > B_dist && last_lead != 1) {
-            lead_changes++;  // A가 앞서게 됨
-            last_lead = 1;
-        } else if (B_dist > A_dist && last_lead != -1) {
-            lead_changes++;  // B가 앞서게 됨
-            last_lead = -1;
-        }
-
-        // A와 B의 남은 시간에서 move_time 만큼 빼준다
-        A_rem_time -= move_time;
-        B_rem_time -= move_time;
-
-        // A의 시간이 다 소진되면 다음 구간으로 넘어감
-        if (A_rem_time == 0) {
-            A_idx++;
-            if (A_idx < N) {
-                A_rem_time = A_time[A_idx];
-            }
-        }
-
-        // B의 시간이 다 소진되면 다음 구간으로 넘어감
-        if (B_rem_time == 0) {
-            B_idx++;
-            if (B_idx < M) {
-                B_rem_time = B_time[B_idx];
-            }
+    scanf("%d %d", &n, &m);
+    
+    int time_a = 1;
+    for(int i = 0; i < n; i++) {
+        int v, t;
+        scanf("%d %d", &v, &t);
+        
+        while(t--) {
+            pos_a[time_a] = pos_a[time_a - 1] + v;
+            time_a++;
         }
     }
-
-    // 결과 출력
-    printf("%d\n", lead_changes);
-
+    
+    int time_b = 1;
+    for(int i = 0; i < m; i++) {
+        int v, t;
+        scanf("%d %d", &v, &t);
+        
+        while(t--) {
+            pos_b[time_b] = pos_b[time_b - 1] + v;
+            time_b++;
+        }
+    }
+    
+    int leader = 0, ans = 0;
+    for(int i = 1; i < time_a; i++) {
+        if(pos_a[i] > pos_b[i]) {
+            if(leader == 2)
+                ans++;
+            leader = 1; 
+        }
+        else if(pos_a[i] < pos_b[i]) {
+            if(leader == 1)
+                ans++;
+            leader = 2; 
+        }
+    }
+    
+    printf("%d\n", ans);
     return 0;
 }
