0d8c6347813393dd37d783e12874f58a19eadb49
1996jessie <jessie1996@naver.com>
2024-10-07 11:53:46 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 241007/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.c
+++ 241007/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.c
@@ -1,55 +1,71 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define MAX_N 100
 #define MAX_T 250
 
+typedef struct {
+    int time;
+    int person1;
+    int person2;
+} Shake;
+
+// 비교 함수 for qsort
+int cmp(const void *a, const void *b) {
+    Shake *shakeA = (Shake *)a;
+    Shake *shakeB = (Shake *)b;
+    return shakeA->time - shakeB->time; // 시간에 따라 오름차순 정렬
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
-    int N, K, P, T;
-
-    // 입력
-    scanf("%d %d %d %d", &N, &K, &P, &T);
-
+    int n, k, p, t;
+    scanf("%d %d %d %d", &n, &k, &p, &t);
+    
     bool infected[MAX_N + 1] = {false}; // 감염된 개발자
-    int infection_count[MAX_N + 1] = {0}; // 각 개발자의 감염 횟수
-    infected[P] = true; // 초기 감염자 설정
+    int shake_num[MAX_N + 1] = {0}; // 각 개발자의 악수 횟수
+    infected[p] = true; // 초기 감염자 설정
 
-    // 악수 정보를 저장하기 위한 배열
-    int time[MAX_T];
-    int x[MAX_T], y[MAX_T];
+    Shake shakes[MAX_T];
 
     // 악수 정보 입력
-    for (int i = 0; i < T; i++) {
-        scanf("%d %d %d", &time[i], &x[i], &y[i]);
+    for (int i = 0; i < t; i++) {
+        scanf("%d %d %d", &shakes[i].time, &shakes[i].person1, &shakes[i].person2);
     }
 
-    // 시간에 따라 악수 처리
-    for (int i = 0; i < T; i++) {
-        int current_x = x[i];
-        int current_y = y[i];
+    // qsort를 사용한 정렬
+    qsort(shakes, t, sizeof(Shake), cmp);
 
-        // x가 감염되어 있는 경우
-        if (infected[current_x] && infection_count[current_x] < K) {
-            if (!infected[current_y]) {
-                infected[current_y] = true; // current_y 감염
-                infection_count[current_x]++; // current_x의 감염 횟수 증가
-            }
+    // 각 악수 횟수를 세고 감염 처리
+    for (int i = 0; i < t; i++) {
+        int target1 = shakes[i].person1;
+        int target2 = shakes[i].person2;
+
+        // 감염되어 있을 경우 악수 횟수를 증가시킵니다.
+        if (infected[target1]) {
+            shake_num[target1]++;
+        }
+        if (infected[target2]) {
+            shake_num[target2]++;
         }
 
-        // y가 감염되어 있는 경우
-        if (infected[current_y] && infection_count[current_y] < K) {
-            if (!infected[current_x]) {
-                infected[current_x] = true; // current_x 감염
-                infection_count[current_y]++; // current_y의 감염 횟수 증가
-            }
+        // target1이 감염되어 있고 아직 K번 이하로 악수했다면 target2를 전염시킵니다.
+        if (shake_num[target1] <= k && infected[target1]) {
+            infected[target2] = true;
+        }
+
+        // target2가 감염되어 있고 아직 K번 이하로 악수했다면 target1을 전염시킵니다.
+        if (shake_num[target2] <= k && infected[target2]) {
+            infected[target1] = true;
         }
     }
 
     // 결과 출력
-    for (int i = 1; i <= N; i++) {
+    for (int i = 1; i <= n; i++) {
         printf("%d", infected[i] ? 1 : 0);
     }
     printf("\n");
+
     return 0;
 }
