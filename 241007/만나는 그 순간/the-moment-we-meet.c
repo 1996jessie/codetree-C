6fa63e463ecbe4f48d135e20a87795bd8dff41db
1996jessie <jessie1996@naver.com>
2024-10-07 11:37:19 +0900

    Title: 만나는 그 순간, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241007/만나는 그 순간/the-moment-we-meet.c
+++ 241007/만나는 그 순간/the-moment-we-meet.c
@@ -22,17 +22,14 @@ int main() {
     int timeA_index = 0, timeB_index = 0;
     int timeA_elapsed = 0, timeB_elapsed = 0;
 
-    // 전체 경과 시간
     int total_time = 0;
 
     while (timeA_index < N && timeB_index < M) {
-        // 이동 시간을 비교하여 같은 시간만큼 진행
         int move_time = timeA[timeA_index] - timeA_elapsed;
         if (timeB[timeB_index] - timeB_elapsed < move_time) {
             move_time = timeB[timeB_index] - timeB_elapsed;
         }
 
-        // A의 방향에 따른 이동
         for (int i = 0; i < move_time; i++) {
             if (dirA[timeA_index] == 'R') posA++;
             else posA--;
@@ -42,18 +39,15 @@ int main() {
 
             total_time++;
 
-            // A와 B가 만나는지 확인
             if (posA == posB) {
                 printf("%d\n", total_time);
                 return 0;
             }
         }
 
-        // 경과 시간 업데이트
         timeA_elapsed += move_time;
         timeB_elapsed += move_time;
 
-        // A와 B의 이동이 끝났다면 다음 구간으로 이동
         if (timeA_elapsed == timeA[timeA_index]) {
             timeA_index++;
             timeA_elapsed = 0;
@@ -64,7 +58,6 @@ int main() {
         }
     }
 
-    // 만난 적이 없을 경우
     printf("-1\n");
     return 0;
 }
