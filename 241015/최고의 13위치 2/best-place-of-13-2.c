3da15a9b7ae63f4876ba080902975909dd25ec22
1996jessie <jessie1996@naver.com>
2024-10-15 19:50:04 +0900

    Title: 최고의 13위치 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241015/최고의 13위치 2/best-place-of-13-2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_N 20
+
+int n;
+int arr[MAX_N][MAX_N];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            scanf("%d", &arr[i][j]);
+    
+    int max_cnt = 0;
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n - 2; j++) {
+            for(int k = 0; k < n; k++) {
+                for(int l = 0; l < n - 2; l++) {
+                    if(i == k && abs(j - l) <= 2)
+                        continue;
+
+                    int cnt1 = arr[i][j] + arr[i][j + 1] + arr[i][j + 2];
+                    int cnt2 = arr[k][l] + arr[k][l + 1] + arr[k][l + 2];
+                    max_cnt = max_cnt > (cnt1 + cnt2) ? max_cnt : (cnt1 + cnt2);
+                }
+            }
+        }
+    
+    printf("%d\n", max_cnt);
+    return 0;
+}
