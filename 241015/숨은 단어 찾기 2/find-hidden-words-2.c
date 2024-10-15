b8a70bb1d66e6a0ad464733000015ad2e21cce74
1996jessie <jessie1996@naver.com>
2024-10-15 19:47:12 +0900

    Title: 숨은 단어 찾기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241015/숨은 단어 찾기 2/find-hidden-words-2.c
+++ 241015/숨은 단어 찾기 2/find-hidden-words-2.c
@@ -1,53 +1,62 @@
 #include <stdio.h>
 #include <string.h>
 
-#define DIR_NUM 4  
+#define MAX_N 100
+#define DIR_NUM 8
 
-int dx[DIR_NUM] = {0, 1, 1, 1};
-int dy[DIR_NUM] = {1, 0, 1, -1};
+int n, m;
+char arr[MAX_N][MAX_N + 1]; 
 
-int InRange(int x, int y, int N, int M) {
-    return 0 <= x && x < N && 0 <= y && y < M;
+int dx[DIR_NUM] = {1, 1, 1, -1, -1, -1, 0, 0};
+int dy[DIR_NUM] = {-1, 0, 1, -1, 0, 1, -1, 1};
+
+int InRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < m;
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int N, M;
-    char grid[50][51];
-
-    scanf("%d %d", &N, &M);
-    for (int i = 0; i < N; i++) {
-        scanf("%s", grid[i]); 
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++) {
+        scanf("%s", arr[i]);
     }
 
-    int count = 0;
+    int cnt = 0;
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (grid[i][j] == 'L') { 
-                for (int k = 0; k < DIR_NUM; k++) {
-                    int x = i, y = j;
-                    int found = 1; 
-                    char target[] = "LEE";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
 
-                    for (int step = 1; step < 3; step++) {
-                        x += dx[k];
-                        y += dy[k];
-                        if (!InRange(x, y, N, M) || grid[x][y] != target[step]) {
-                            found = 0;  
-                            break;
-                        }
-                    }
+            if (arr[i][j] != 'L') 
+                continue;
 
-                    if (found) {
-                        count++;  
-                    }
+            for (int k = 0; k < DIR_NUM; k++) {
+                int curt = 1;
+                int curx = i;
+                int cury = j;
+
+                while (1) {
+                    int nx = curx + dx[k];
+                    int ny = cury + dy[k];
+
+                    if (!InRange(nx, ny))
+                        break;
+
+                    if (arr[nx][ny] != 'E')
+                        break;
+
+                    curt++; 
+                    curx = nx;  
+                    cury = ny;
+                }
+
+                if (curt >= 3) {
+                    cnt++;
                 }
             }
         }
     }
 
-    printf("%d\n", count);
-    
+    printf("%d\n", cnt);
+
     return 0;
 }
