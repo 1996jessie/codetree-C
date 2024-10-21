f3e9e81b164aecb0aedd2aec5a6de31b633216ec
1996jessie <jessie1996@naver.com>
2024-10-21 14:34:33 +0900

    Title: 좌표평면 위의 균형 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241021/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.c
+++ 241021/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.c
@@ -7,21 +7,19 @@ int x[MAX_N], y[MAX_N];
 
 int calculate_max_points_in_zone(int x_line, int y_line) {
     int zone[4] = {0, 0, 0, 0}; 
-    
-    // 각 점을 해당하는 구역에 분배
+
     for (int i = 0; i < n; i++) {
         if (x[i] > x_line && y[i] > y_line) {
-            zone[0]++; // 1사분면
+            zone[0]++; 
         } else if (x[i] < x_line && y[i] > y_line) {
-            zone[1]++; // 2사분면
+            zone[1]++; 
         } else if (x[i] < x_line && y[i] < y_line) {
-            zone[2]++; // 3사분면
+            zone[2]++; 
         } else {
-            zone[3]++; // 4사분면
+            zone[3]++; 
         }
     }
 
-    // 4개의 구역 중 가장 큰 구역의 점 개수 찾기
     int max_points = zone[0];
     for (int i = 1; i < 4; i++) {
         if (zone[i] > max_points) {
@@ -39,21 +37,17 @@ int main() {
         scanf("%d %d", &x[i], &y[i]);
     }
 
-    int min_max_points = n; // 가장 큰 구역의 점의 최소값을 찾기 위한 초기값
+    int min_max_points = n;
 
-    // 가능한 모든 짝수 x_line과 y_line에 대해 검사
     for (int x_line = 2; x_line <= 100; x_line += 2) {
         for (int y_line = 2; y_line <= 100; y_line += 2) {
-            // 각 조합에 대해 가장 큰 구역의 점 개수를 계산
             int max_points = calculate_max_points_in_zone(x_line, y_line);
-            // 그 중 최소값을 찾음
             if (max_points < min_max_points) {
                 min_max_points = max_points;
             }
         }
     }
 
-    // 결과 출력
     printf("%d\n", min_max_points);
 
     return 0;
