a8cf0b669f028c7297df3c65233c54f1dc82c2ac
1996jessie <jessie1996@naver.com>
2024-10-21 14:38:05 +0900

    Title: 팀으로 하는 틱택토 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241021/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.c
+++ 241021/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.c
@@ -17,17 +17,11 @@ int main() {
 
     int ans = 0;
 
-    // 모든 쌍에 대해 전부 시도해 봅니다.
     for(int i = 1; i <= MAX_X; i++) {
         for(int j = i + 1; j <= MAX_X; j++) {
-            // i, j 두 명이 팀이 됐을 때 이길 수 있는지를 확인합니다.
-            // win : 두 명이 팀이 됐을 때 이길 수 있다면 true
             int win = 0;
-
-            // num_i , num_j : 각 줄에 i, j가 나오는 개수
             int num_i = 0, num_j = 0;
 
-            // 가로로 빙고가 만들어질 때
             for(int k = 0; k < MAX_A; k++) {
                 num_i = 0, num_j = 0;
                 for(int l = 0; l < MAX_A; l++) {
@@ -37,13 +31,10 @@ int main() {
                         num_j++;
                 }
 
-                // 3개의 칸에 i, j가 총 3번 나오며
-                // 둘 다 최소 1번 이상 나오면 승리
                 if(num_i + num_j == 3 && num_i >= 1 && num_j >= 1)
                     win = 1;
             }
 
-            // 세로로 빙고가 만들어질 때
             for(int k = 0; k < MAX_A; k++) {
                 num_i = 0, num_j = 0;
                 for(int l = 0; l < MAX_A; l++) {
@@ -53,13 +44,10 @@ int main() {
                         num_j++;
                 }
 
-                // 3개의 칸에 i, j가 총 3번 나오며
-                // 둘 다 최소 1번 이상 나오면 승리
                 if(num_i + num_j == 3 && num_i >= 1 && num_j >= 1)
                     win = 1;
             }
 
-            // 왼쪽 위에서 오른쪽 아래를 잇는 대각선으로 빙고가 만들어질 때
             num_i = 0, num_j = 0;
             for(int k = 0; k < MAX_A; k++) {
                 if(board[k][k] == i)
@@ -68,12 +56,9 @@ int main() {
                     num_j++;
             }
 
-            // 3개의 칸에 i, j가 총 3번 나오며
-            // 둘 다 최소 1번 이상 나오면 승리
             if(num_i + num_j == 3 && num_i >= 1 && num_j >= 1)
                 win = 1;
-
-            // 오른쪽 위에서 왼쪽 아래를 잇는 대각선으로 빙고가 만들어질 때
+                
             num_i = 0, num_j = 0;
             for(int k = 0; k < MAX_A; k++) {
                 if(board[k][MAX_A - k - 1] == i)
@@ -81,9 +66,6 @@ int main() {
                 if(board[k][MAX_A - k - 1] == j)
                     num_j++;
             }
-
-            // 3개의 칸에 i, j가 총 3번 나오며
-            // 둘 다 최소 1번 이상 나오면 승리
             if(num_i + num_j == 3 && num_i >= 1 && num_j >= 1)
                 win = 1;
 
