ecd332dfcb731b50340c2ac68dcd43653ca611e8
1996jessie <jessie1996@naver.com>
2024-10-23 20:10:47 +0900

    Title: 초기 수열 복원하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241023/초기 수열 복원하기/restore-initial-sequence.c
+++ 241023/초기 수열 복원하기/restore-initial-sequence.c
@@ -5,19 +5,18 @@
 
 int N;
 int sum[MAX_N];
-int A[MAX_N];    // 복원할 수열
+int A[MAX_N];
 
-bool used[MAX_N + 1];  // 숫자가 사용되었는지 체크하는 배열
+bool used[MAX_N + 1];  
 
 bool is_valid(int A1) {
     A[0] = A1;
     used[A1] = true;
     
-    // 수열의 나머지 원소들을 복원
     for (int i = 1; i < N; i++) {
-        A[i] = sum[i - 1] - A[i - 1];  // A[i]는 sum[i-1] - A[i-1]로 구함
+        A[i] = sum[i - 1] - A[i - 1];  
         if (A[i] < 1 || A[i] > N || used[A[i]]) {
-            return false;  // 범위를 벗어나거나 이미 사용된 숫자인 경우
+            return false;  
         }
         used[A[i]] = true;
     }
@@ -38,11 +37,9 @@ int main() {
         scanf("%d", &sum[i]);
     }
     
-    // 첫 번째 원소 A[0]을 1부터 N까지 시도
     for (int A1 = 1; A1 <= N; A1++) {
-        reset_used();  // used 배열을 초기화
+        reset_used(); 
         if (is_valid(A1)) {
-            // 유효한 수열을 찾은 경우 출력
             for (int i = 0; i < N; i++) {
                 printf("%d ", A[i]);
             }
