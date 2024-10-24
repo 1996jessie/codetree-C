683594fb8f5192aec7a6635737355b07a8af7f31
1996jessie <jessie1996@naver.com>
2024-10-24 23:11:38 +0900

    Title: 구역 청소, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241024/구역 청소/district-cleaning.c
+++ 241024/구역 청소/district-cleaning.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
+int a, b, c, d;
+
+int Intersecting(int x1, int x2, int x3, int x4) {
+    if (x2 < x3 || x4 < x1)
+        return 0;
+    else
+        return 1;
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a, b;
-    int c, d;
-
     scanf("%d %d", &a, &b);
     scanf("%d %d", &c, &d);
 
-    int start = (a < c) ? a : c;
-    int end = (b > d) ? b : d;
-
-    int cleaned = end - start;
-
-    if (cleaned < 0) {
-        cleaned = 0;
+    if (Intersecting(a, b, c, d)) {
+        printf("%d\n", (b > d ? b : d) - (a < c ? a : c));
+    } else {
+        printf("%d\n", (b - a) + (d - c));
     }
-
-    printf("%d\n", cleaned);
 
     return 0;
 }
