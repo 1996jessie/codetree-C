17dbaa007ee7847f5fd6808652ade06f6524756f
1996jessie <jessie1996@naver.com>
2024-10-25 21:26:56 +0900

    Title: X 달리기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241025/X 달리기/x-run.c
+++ 241025/X 달리기/x-run.c
@@ -2,31 +2,28 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int X;
-    scanf("%d", &X);
+    int distance;
+    scanf("%d", &distance);
 
+    int max_speed = 0;
     int time = 0;
-    int distance = 0;
-    int speed = 1;
+    int have_to_move = 0;
+    while (1) {
+        max_speed++;
+        int necessary_moved = (max_speed * max_speed);
 
-    while (distance < X) {
-        time++;
-        distance += speed;
+        if (necessary_moved > distance) break;
 
-        if (distance >= X) {
-            break;
-        }
-        if (speed < time) {
-            speed++;
-        }
+        time = max_speed * 2 - 1;
+
+        have_to_move = distance - necessary_moved;
+
+        time += (have_to_move / max_speed);
+
+        if (have_to_move % max_speed != 0)
+            time++;
     }
-
-    while (distance > X) {
-        speed--;
-        distance -= speed;
-        time++;
-    }
-
     printf("%d\n", time);
+
     return 0;
 }
