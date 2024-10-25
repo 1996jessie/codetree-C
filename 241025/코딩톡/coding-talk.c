1a244b5ac7dab64f4f4acbef54faf564fad87df6
1996jessie <jessie1996@naver.com>
2024-10-25 21:25:33 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241025/코딩톡/coding-talk.c
+++ 241025/코딩톡/coding-talk.c
@@ -1,51 +1,31 @@
 #include <stdio.h>
-#include <string.h>
 
-#define MAX_N 26
 #define MAX_M 100
+
+int n, p, m;
+char c[MAX_M];
+int u[MAX_M];
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n, m, p;
-    char messages[MAX_M][2]; // 메세지 정보를 저장 (사람, 읽지 않은 수)
-    int unreadCount[MAX_M];   // 각 메세지에 대한 읽지 않은 사람 수
-    int read[MAX_N] = {0};    // 사람들의 읽음 여부 (0: 읽지 않음, 1: 읽음)
-
     scanf("%d %d %d", &n, &m, &p);
-    for (int i = 0; i < m; i++) {
-        scanf(" %c %d", &messages[i][0], &unreadCount[i]);
-    }
+    for(int i = 0; i < m; i++)
+        scanf(" %c %d", &c[i], &u[i]);
 
-    // p번째 메세지를 보낸 사람
-    char sender = messages[p - 1][0];
+    if(u[p - 1] == 0)
+        return 0;
 
-    // p번째 메세지 이후의 모든 메세지 확인
-    for (int i = p; i < m; i++) {
-        read[messages[i][0] - 'A'] = 1; // 해당 메세지를 보낸 사람은 읽음
-    }
+    for(int i = 0; i < n; i++) {
+        char person = 'A' + i;
+        int read = 0;
 
-    // p번째 메세지에 대한 읽지 않은 사람 수에 따라 처리
-    for (int i = 0; i < p - 1; i++) {
-        int unread = unreadCount[i];
-        for (int j = 0; j < unread; j++) {
-            read[messages[i][0] - 'A'] = 1; // 읽지 않은 사람으로 표시
-        }
-    }
+        for(int j = 0; j < m; j++)
+            if(u[j] >= u[p - 1] && c[j] == person)
+                read = 1;
 
-    // 가능한 사람들을 확인
-    int possible[MAX_N];
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-        if (!read[i]) {
-            possible[count++] = i; // 읽지 않았던 사람
-        }
+        if(read == 0)
+            printf("%c ", person);
     }
-
-    // 결과 출력
-    for (int i = 0; i < count; i++) {
-        printf("%c ", 'A' + possible[i]);
-    }
-    printf("\n");
 
     return 0;
 }
