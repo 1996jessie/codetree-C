d0058e5deebc14606c750c77ce765ecddf93d09d
1996jessie <jessie1996@naver.com>
2024-10-26 10:36:23 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241026/연속된 숫자 만들기 2/create-consecutive-numbers-2.c
+++ 241026/연속된 숫자 만들기 2/create-consecutive-numbers-2.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_N 3
+
 int compare(const void *a, const void *b) {
     return *(int*)a - *(int*)b;
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int positions[3];
-    scanf("%d %d %d", &positions[0], &positions[1], &positions[2]);
-    
-    qsort(positions, 3, sizeof(int), compare);
+    int n = 3;
+    int a[MAX_N];
 
-    int A = positions[0], B = positions[1], C = positions[2];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
 
-    if (B - A == 1 && C - B == 1)
-        printf("0\n");
-    else if (B - A <= 2 || C - B <= 2)
-        printf("1\n");
+    qsort(a, n, sizeof(int), compare);
+
+    if(a[0] + 1 == a[1] && a[1] + 1 == a[2])
+        printf("0");
+    else if(a[0] + 2 == a[1] || a[1] + 2 == a[2])
+        printf("1");
     else
-        printf("2\n");
+        printf("2");
 
     return 0;
 }
