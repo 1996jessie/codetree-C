cd1281cd508644a392274d424df7fabe23349fd8
1996jessie <jessie1996@naver.com>
2024-10-26 10:46:10 +0900

    Title: 연속된 숫자 만들기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241026/연속된 숫자 만들기 3/create-consecutive-numbers-3.c
+++ 241026/연속된 숫자 만들기 3/create-consecutive-numbers-3.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
+#define MAX_N 3
+
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
+    int a[MAX_N];
+    
+    for(int i = 0; i < MAX_N; i++)
+        scanf("%d", &a[i]);
 
-    int max_moves = 0;
-
-    if (b - a > 1 && c - b > 1) {
-        max_moves = (b - a - 1) + (c - b - 1);
-    } else if (b - a > 1) {
-        max_moves = (b - a - 1);
-    } else if (c - b > 1) {
-        max_moves = (c - b - 1);
+    if(a[0] + 1 == a[1] && a[1] + 1 == a[2]) {
+        printf("0\n");
+        return 0;
     }
+    
+    int max_move = 0;
+    int move = a[2] - a[1] - 1;
+    max_move = (max_move > move) ? max_move : move;
 
-    max_moves += 2;
+    move = a[1] - a[0] - 1;
+    max_move = (max_move > move) ? max_move : move;
 
-    printf("%d\n", max_moves);
+    printf("%d\n", max_move);
+    
     return 0;
 }
