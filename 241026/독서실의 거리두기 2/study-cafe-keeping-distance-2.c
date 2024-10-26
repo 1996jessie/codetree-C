58a049b3df1aa695b32b420a99eff39beb4e7be1
1996jessie <jessie1996@naver.com>
2024-10-26 10:48:25 +0900

    Title: 독서실의 거리두기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241026/독서실의 거리두기 2/study-cafe-keeping-distance-2.c
+++ 241026/독서실의 거리두기 2/study-cafe-keeping-distance-2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 
 #define MAX_N 1000
 
@@ -11,26 +12,68 @@ int main() {
     scanf("%d", &n);
     scanf("%s", seats);
 
-    int max_distance = 0;
-    int last_one = -1;
-
+    int max_dist = 0;
+    int max_i, max_j;
     for (int i = 0; i < n; i++) {
         if (seats[i] == '1') {
-            if (last_one == -1) {
-                int distance = i;
-                max_distance = (distance + 1) / 2;
-            } else {
-                int distance = i - last_one;
-                max_distance = (max_distance > (distance + 1) / 2) ? max_distance : (distance + 1) / 2;
+            for (int j = i + 1; j < n; j++) {
+                if (seats[j] == '1') {
+                    if (j - i > max_dist) {
+                        max_dist = j - i;
+                        max_i = i;
+                        max_j = j;
+                    }
+                    break;
+                }
             }
-            last_one = i;
         }
     }
 
-    int distance = n - last_one - 1;
-    max_distance = (max_distance > distance) ? max_distance : distance;
+    int max_dist2 = -1;
+    int max_idx = -1;
+    if (seats[0] == '0') {
+        int dist = 0;
+        for (int i = 0; i < n; i++) {
+            if (seats[i] == '1')
+                break;
+            dist++;
+        }
+        if (dist > max_dist2) {
+            max_dist2 = dist;
+            max_idx = 0;
+        }
+    }
 
-    printf("%d\n", max_distance);
-    
+    if (seats[n - 1] == '0') {
+        int dist = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (seats[i] == '1')
+                break;
+            dist++;
+        }
+        if (dist > max_dist2) {
+            max_dist2 = dist;
+            max_idx = n - 1;
+        }
+    }
+
+    if (max_dist2 >= max_dist / 2)
+        seats[max_idx] = '1';
+    else
+        seats[(max_i + max_j) / 2] = '1';
+
+    int ans = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        if (seats[i] == '1') {
+            for (int j = i + 1; j < n; j++) {
+                if (seats[j] == '1') {
+                    ans = (ans < j - i) ? ans : j - i;
+                    break;
+                }
+            }
+        }
+    }
+
+    printf("%d\n", ans);
     return 0;
 }
