7990b3529492cfdc3bda2d318f8cb7081c89d688
1996jessie <jessie1996@naver.com>
2024-10-26 10:41:14 +0900

    Title: 독서실의 거리두기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241026/독서실의 거리두기 3/study-cafe-keeping-distance-3.c
+++ 241026/독서실의 거리두기 3/study-cafe-keeping-distance-3.c
@@ -1,45 +1,50 @@
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 
 #define MAX_N 1000
 
+int n;
+char seats[MAX_N + 1];
+
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n;
-    char seats[MAX_N + 1];
-    
-    // 입력
     scanf("%d", &n);
     scanf("%s", seats);
-    
-    int max_min_dist = 0; 
+
+    int max_dist = 0;
+    int max_i, max_j;
     for (int i = 0; i < n; i++) {
-        if (seats[i] == '0') {
-            int left_dist = n, right_dist = n;
-            
-            for (int j = i - 1; j >= 0; j--) {
-                if (seats[j] == '1') {
-                    left_dist = i - j;
-                    break;
-                }
-            }
-            
+        if (seats[i] == '1') {
             for (int j = i + 1; j < n; j++) {
                 if (seats[j] == '1') {
-                    right_dist = j - i;
+                    if (j - i > max_dist) {
+                        max_dist = j - i;
+                        max_i = i;
+                        max_j = j;
+                    }
                     break;
                 }
-            }
-            
-            int min_dist = (left_dist < right_dist) ? left_dist : right_dist;
-
-            if (min_dist > max_min_dist) {
-                max_min_dist = min_dist;
             }
         }
     }
 
-    printf("%d\n", max_min_dist);
-    
+    seats[(max_i + max_j) / 2] = '1';
+
+    int ans = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        if (seats[i] == '1') {
+            for (int j = i + 1; j < n; j++) {
+                if (seats[j] == '1') {
+                    if (j - i < ans) {
+                        ans = j - i;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    printf("%d\n", ans);
     return 0;
 }
