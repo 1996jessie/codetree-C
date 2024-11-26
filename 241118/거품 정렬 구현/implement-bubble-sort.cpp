24f82d833becd3a52d4fb87c3cb4d6c7620f9905
1996jessie <jessie1996@naver.com>
2024-11-26 16:41:45 +0900

    commit

--- 241118/거품 정렬 구현/implement-bubble-sort.cpp
+++ 241118/거품 정렬 구현/implement-bubble-sort.cpp
@@ -1,30 +1,6 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n;
-    cin >> n;
-    
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
     return 0;
 }
