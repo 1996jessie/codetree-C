039a1bca1929f63134e9ff7b9abf1c029cdd9128
1996jessie <jessie1996@naver.com>
2024-11-26 16:21:31 +0900

    Update golden-toast.cpp

--- 241114/황금비율 토스트/golden-toast.cpp
+++ 241114/황금비율 토스트/golden-toast.cpp
@@ -1,48 +1,133 @@
-#include <iostream>
-#include <string>
-#include <list>
- 
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int n, m;
+typedef struct Node {
+    char data;
+    struct Node *prev;
+    struct Node *next;
+} Node;
 
-string s;
+typedef struct {
+    Node *head;
+    Node *tail;
+    Node *cursor;
+} List;
+
+void initList(List *list) {
+    list->head = NULL;
+    list->tail = NULL;
+    list->cursor = NULL;
+}
+
+void insert(List *list, char c) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    newNode->data = c;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+
+    if (list->cursor == NULL) {
+        list->head = newNode;
+        list->tail = newNode;
+        list->cursor = newNode;
+    } else {
+        if (list->cursor == list->tail) {
+            newNode->prev = list->tail;
+            list->tail->next = newNode;
+            list->tail = newNode;
+        } else {
+            newNode->prev = list->cursor;
+            newNode->next = list->cursor->next;
+            if (list->cursor->next) {
+                list->cursor->next->prev = newNode;
+            }
+            list->cursor->next = newNode;
+            if (list->cursor == list->tail) {
+                list->tail = newNode;
+            }
+        }
+    }
+    list->cursor = newNode;
+}
+
+void delete(List *list) {
+    if (list->cursor == NULL) return;
+
+    Node *toDelete = list->cursor;
+
+    if (toDelete == list->head && toDelete == list->tail) {
+        list->head = NULL;
+        list->tail = NULL;
+        list->cursor = NULL;
+    } else if (toDelete == list->head) {
+        list->head = toDelete->next;
+        list->head->prev = NULL;
+        list->cursor = list->head;
+    } else if (toDelete == list->tail) {
+        list->tail = toDelete->prev;
+        list->tail->next = NULL;
+        list->cursor = NULL;
+    } else {
+        toDelete->prev->next = toDelete->next;
+        toDelete->next->prev = toDelete->prev;
+        list->cursor = toDelete->next;
+    }
+    free(toDelete);
+}
+
+void moveLeft(List *list) {
+    if (list->cursor && list->cursor->prev) {
+        list->cursor = list->cursor->prev;
+    }
+}
+
+void moveRight(List *list) {
+    if (list->cursor && list->cursor->next) {
+        list->cursor = list->cursor->next;
+    }
+}
+
+void printList(List *list) {
+    Node *current = list->head;
+    while (current) {
+        printf("%c", current->data);
+        current = current->next;
+    }
+}
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    cin >> n >> m;
-    cin >> s;
-    
-    list<char> l;
-    for(int i = 0; i < s.length(); i++)
-        l.push_back(s[i]);  
-    list<char>::iterator it = l.end();
-    
-    while(m--) { 
+    int n, m;
+    char s[1000001];
+
+    scanf("%d %d", &n, &m);
+    scanf("%s", s);
+
+    List list;
+    initList(&list);
+
+    for (int i = 0; i < strlen(s); i++) {
+        insert(&list, s[i]);
+    }
+    list.cursor = list.tail;
+
+    while (m--) {
         char command;
-        cin >> command;
-        
-        if(command == 'L') {
-            if(it != l.begin())  
-                it--;    
-        } 
-        else if(command == 'R') {
-            if(it != l.end())   
-                it++;       
-        }
-        else if(command == 'D') {
-            if(it != l.end())   
-                it = l.erase(it); 
-        }
-        else if(command == 'P') {
+        scanf(" %c", &command);
+
+        if (command == 'L') {
+            moveLeft(&list);
+        } else if (command == 'R') {
+            moveRight(&list);
+        } else if (command == 'D') {
+            delete(&list);
+        } else if (command == 'P') {
             char c;
-            cin >> c;
-            l.insert(it, c);   
-        }    
+            scanf(" %c", &c);
+            insert(&list, c);
+        }
     }
-    
-    for (list<char>::iterator it = l.begin(); it != l.end(); it++) {
-        cout << *it;
-    }
-    return 0;     
+
+    printList(&list);
+    return 0;
 }
