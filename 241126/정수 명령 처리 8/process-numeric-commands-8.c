0704b6e6aa366ea084d034a2a66fb8e9406c4ed6
1996jessie <jessie1996@naver.com>
2024-11-26 11:43:05 +0900

    Title: 정수 명령 처리 8, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241126/정수 명령 처리 8/process-numeric-commands-8.c
+++ 241126/정수 명령 처리 8/process-numeric-commands-8.c
@@ -2,28 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-// 노드 구조체 정의
 typedef struct Node {
     int value;
     struct Node* prev;
     struct Node* next;
 } Node;
 
-// 이중 연결 리스트 구조체 정의
 typedef struct DoublyLinkedList {
     Node* head;
     Node* tail;
     int size;
 } DoublyLinkedList;
 
-// 이중 연결 리스트 초기화
 void init_list(DoublyLinkedList* list) {
     list->head = NULL;
     list->tail = NULL;
     list->size = 0;
 }
 
-// push_front 함수
 void push_front(DoublyLinkedList* list, int value) {
     Node* new_node = (Node*)malloc(sizeof(Node));
     new_node->value = value;
@@ -39,7 +35,6 @@ void push_front(DoublyLinkedList* list, int value) {
     list->size++;
 }
 
-// push_back 함수
 void push_back(DoublyLinkedList* list, int value) {
     Node* new_node = (Node*)malloc(sizeof(Node));
     new_node->value = value;
@@ -55,7 +50,6 @@ void push_back(DoublyLinkedList* list, int value) {
     list->size++;
 }
 
-// pop_front 함수
 void pop_front(DoublyLinkedList* list) {
     if (list->size == 0) return;
 
@@ -73,7 +67,6 @@ void pop_front(DoublyLinkedList* list) {
     list->size--;
 }
 
-// pop_back 함수
 void pop_back(DoublyLinkedList* list) {
     if (list->size == 0) return;
 
@@ -91,41 +84,37 @@ void pop_back(DoublyLinkedList* list) {
     list->size--;
 }
 
-// size 함수
 void get_size(const DoublyLinkedList* list) {
     printf("%d\n", list->size);
 }
 
-// empty 함수
 void is_empty(const DoublyLinkedList* list) {
     printf("%d\n", list->size == 0 ? 1 : 0);
 }
 
-// front 함수
 void get_front(const DoublyLinkedList* list) {
     if (list->size == 0) return;
     printf("%d\n", list->head->value);
 }
 
-// back 함수
 void get_back(const DoublyLinkedList* list) {
     if (list->size == 0) return;
     printf("%d\n", list->tail->value);
 }
 
-// 메인 함수
 int main() {
+    // 여기에 코드를 작성해주세요.
     DoublyLinkedList list;
     init_list(&list);
 
     int N;
     scanf("%d", &N);
-    getchar(); // 개행 문자 처리
+    getchar();
 
     for (int i = 0; i < N; i++) {
         char command[100];
         fgets(command, sizeof(command), stdin);
-        command[strcspn(command, "\n")] = '\0'; // 개행 문자 제거
+        command[strcspn(command, "\n")] = '\0';
 
         if (strncmp(command, "push_front", 10) == 0) {
             int value = atoi(command + 11);
