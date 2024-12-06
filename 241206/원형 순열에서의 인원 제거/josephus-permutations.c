876bcf4d08cec9d64346353d145644d2f2e9f11a
1996jessie <jessie1996@naver.com>
2024-12-06 09:26:42 +0900

    Title: 원형 순열에서의 인원 제거, Time: 6ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 241206/원형 순열에서의 인원 제거/josephus-permutations.c
+++ 241206/원형 순열에서의 인원 제거/josephus-permutations.c
@@ -2,26 +2,52 @@
 
 #define MAX 5000
 
-int n, k;
 int queue[MAX];
 int front = 0, back = 0;
 
+void push(int item) {
+    queue[back++] = item;
+}
+
+int empty() {
+    return front == back;
+}
+
+int size() {
+    return back - front;
+}
+
+int pop() {
+    if (empty()) {
+        return -1;
+    }
+    return queue[front++];
+}
+
+int front_val() {
+    if (empty()) {
+        return -1;
+    }
+    return queue[front];
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
+    int n, k;
     scanf("%d %d", &n, &k);
 
-    for(int i = 0; i < n; i++) {
-        queue[back++] = i + 1;
+    for (int i = 1; i <= n; i++) {
+        push(i);
     }
 
-    while(front < back - 1) {
-        for(int i = 0; i < k - 1; i++) {
-            queue[back++] = queue[front++];
+    while (size() > 1) {
+        for (int i = 0; i < k - 1; i++) {
+            push(front_val());
+            pop();
         }
-        printf("%d ", queue[front++]);
+        printf("%d ", pop());
     }
 
-    printf("%d\n", queue[front]);
-
+    printf("%d ", pop());
     return 0;
 }
